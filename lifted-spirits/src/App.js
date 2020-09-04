fec97db5b1063f3f720ec4b661b10027862e4660
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-09-04 11:57:16 -0700

    Working API

--- lifted-spirits/src/App.js
+++ lifted-spirits/src/App.js
@@ -1,8 +1,29 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+// import Activities from './Activities';
+// import Dashboard from './Dashboard';
+// import Journal from './Journal';
+// import Mood from './Mood';
+// import Summary from './Summary';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [userInfo, setUserInfo] = useState([]);
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      // Base and key aren't accessible for some reason
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/mood/?Grid%20View`
+      const response = await axios.get(airtableURL, {
+        headers: {
+          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        }
+      });
+      console.log(response.data.records)
+      setUserInfo(response.data.records);
+    }
+    getUserInfo();
+  }, []);
   return (
     <div className="App">
      
---
+++ lifted-spirits/src/Components/Mood.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Mood = (props) => {
+  const { }
+  return (
+
+  )
+}
+
+export default Mood;
