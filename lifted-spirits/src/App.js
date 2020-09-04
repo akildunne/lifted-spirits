e5fe7d031edb0f6310b673ad40b1277a572f5ad0
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-09-04 08:30:38 -0700

    Initialize App

---
+++ lifted-spirits/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
