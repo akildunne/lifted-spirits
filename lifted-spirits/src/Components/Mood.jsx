11cbaa85465ff29a6012e43d0c7eb3adab7ada1b
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-09-06 20:50:36 -0700

    Mood Icons Added, Still Errors

--- lifted-spirits/src/Components/Mood.jsx
+++ lifted-spirits/src/Components/Mood.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import React from "react";
+//Not sure if this is needed, but it said to add it in font awesome
+// import ReactDOM from 'react-dom'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-// library.add(faUserAstronaut)
+library.add(
+  "fa-smile",
+  "fa-frown",
+  "fa-meh",
+  "fa-laugh-squint",
+  "fa-angry",
+  "fa-meh-rolling-eyes"
+);
+
+const smile = icon({ prefix: "fas", iconName: "fa-smile" });
+const sad = icon({ prefix: "fas", iconName: "fa-frown" });
+const meh = icon({ prefix: "fas", iconName: "fa-meh" });
+const excited = icon({ prefix: "fas", iconName: "fa-laugh-squint" });
+const angry = icon({ prefix: "fas", iconName: "fa-angry" });
+const annoyed = icon({ prefix: "fas", iconName: "fa-meh-rolling-eyes" });
 
 const Mood = () => {
-  const moods = ['happy', 'sad', 'meh', 'excited', 'anxious', 'annoyed']
+  const moods = ["happy", "sad", "meh", "excited", "angry", "annoyed"];
   return (
-        <div>
-        {moods.map(mood => (
-        if (mood === 'happy') {
-        <img></img>
-      } else if(mood === 'sad') {
-        <img src=></img>
-      } else if(mood === 'meh') {
-        <img></img>
-      } else if(mood === 'excited') {
-        <img></img>
-      } else if(mood === 'anxious') {
-        <img></img>
-      } else (mood === 'annoyed') {
-        <img></img>
-        }
-        
-        ))}
-        </div>
-  )
-}
+    <div>
+      {moods.map(mood => ( if(mood === 'happy') {{ smile }} else if(mood ===
+      'sad') {{ sad }} else if(mood === 'meh') {{ meh }} else if(mood ===
+      'excited') {{ excited }} else if(mood === 'angry') {{ angry }} else{" "}
+      {{ annoyed }}
+      ))}
+    </div>
+  );
+};
 
-
-dom.watch()
 export default Mood;
