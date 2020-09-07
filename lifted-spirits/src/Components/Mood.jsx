7af4efe4bbf98c1078557aeda6710c1e50e50775
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-09-07 10:03:37 -0700

    Working Mood

--- lifted-spirits/src/Components/Mood.jsx
+++ lifted-spirits/src/Components/Mood.jsx
@@ -1,34 +1,23 @@
 import React from "react";
-//Not sure if this is needed, but it said to add it in font awesome
-// import ReactDOM from 'react-dom'
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
 
-library.add(
-  "fa-smile",
-  "fa-frown",
-  "fa-meh",
-  "fa-laugh-squint",
-  "fa-angry",
-  "fa-meh-rolling-eyes"
-);
-
-const smile = icon({ prefix: "fas", iconName: "fa-smile" });
-const sad = icon({ prefix: "fas", iconName: "fa-frown" });
-const meh = icon({ prefix: "fas", iconName: "fa-meh" });
-const excited = icon({ prefix: "fas", iconName: "fa-laugh-squint" });
-const angry = icon({ prefix: "fas", iconName: "fa-angry" });
-const annoyed = icon({ prefix: "fas", iconName: "fa-meh-rolling-eyes" });
 
 const Mood = () => {
-  const moods = ["happy", "sad", "meh", "excited", "angry", "annoyed"];
+  const moods = [
+    { icon: <i className="fas fa-smile"></i>, name: 'happy' },
+    { icon: <i className="fas fa-frown"></i>, name: 'sad' },
+    { icon: <i className="fas fa-meh"></i>, name: 'meh' },
+    { icon: <i className="fas fa-laugh-squint"></i>, name: 'excited' },
+    { icon: <i className="fas fa-angry"></i>, name: 'angry' },
+    { icon: <i className="fas fa-meh-rolling-eyes"></i>, name: 'annoyed' }]
+  
+  const handleSubmit = (name) => {
+    console.log(name)
+  }
+
   return (
+    
     <div>
-      {moods.map(mood => ( if(mood === 'happy') {{ smile }} else if(mood ===
-      'sad') {{ sad }} else if(mood === 'meh') {{ meh }} else if(mood ===
-      'excited') {{ excited }} else if(mood === 'angry') {{ angry }} else{" "}
-      {{ annoyed }}
-      ))}
+      {moods.map(mood => <div onClick={(e) => handleSubmit(mood.name)}>{mood.icon}</div>)}
     </div>
   );
 };
